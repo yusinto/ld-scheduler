2c7253d577676676498e93e16da2c707107be336
Yusinto Ngadiman <yusinto@gmail.com>
2017-02-24 22:30:21 +1100

    Implemented deployment scheduling. Added polling mechanism.

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,6 +1,16 @@
+import moment from 'moment';
 
-const apiKey = 'secret-api-key';
+const updateList = [
+  {
+    flag: 'dev-test-flag',
+    value: true,
+    deploymentDateTime: '2017-02-24 22:27',
+    isDeployed: false,
+  },
+];
+const apiKey = 'your-api-key';
 const project = 'default';
+const environment = 'test';
 const baseUrl = `https://app.launchdarkly.com/api/v2/flags/${project}`;
 const headers = {
   Accept: '*/*',
@@ -9,38 +19,63 @@ const headers = {
   'accept-encoding': 'gzip, deflate'
 };
 
-const updateFlag = async (flag) => {
-  console.log('Running updateFlag...');
+// set said flag to the specified value
+const run = () => {
+  console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} lds is waking up...`);
 
-  // GOTCHA: Must stringify body!!!!
-  const body = JSON.stringify([
-    {"op": "replace", "path": "/environments/test/on", "value": true}
-  ]);
+  // process un-deployed items only
+  const undeployedFlags = updateList.filter(f => !f.isDeployed && moment().isAfter(moment(f.deploymentDateTime, 'YYYY-MM-DD HH:mm')));
 
-  const url = `${baseUrl}/${flag}`;
-  const response = await fetch(url, {
-    method: 'PATCH',
-    headers,
-    body,
+  if(undeployedFlags.length === 0) {
+    console.log('Nothing to process, going back to sleep');
+    return;
+  }
+
+  undeployedFlags.forEach(async(f) => {
+    const {flag, value} = f;
+    console.log(`Processing ${JSON.stringify(f)}`);
+
+    // GOTCHA: Must stringify body!!!!
+    const body = JSON.stringify([{
+      op: 'replace',
+      path: `/environments/${environment}/on`,
+      value,
+    }]);
+
+    const url = `${baseUrl}/${flag}`;
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers,
+      body,
+    });
+
+    console.log(`Response: ${response.status} ${response.statusText} from: ${response.url}`);
+
+    if (response.status === 200) {
+      f.isDeployed = true;
+      console.log(`Successfully updated ${flag} to ${value}. `);
+    } else {
+      console.log(`Error occurred. Did not update ${flag}. Will retry again later.`);
+    }
   });
-
-  console.log(`response: ${response.status} ${response.statusText} from: ${response.url}`);
 };
 
-const listFlags = async () => {
-  console.log('Running listFlags...');
+// list all flags in the current project
+// const listFlags = async() => {
+//   console.log('Running listFlags...');
+//
+//   const url = `${baseUrl}?env=${environment}`;
+//   const response = await fetch(url, {
+//     method: 'GET',
+//     headers,
+//   });
+//
+//   // GOTCHA: must use .json() to convert response.body to json!!!
+//   const data = await response.json();
+//   console.log(`response: ${response.status} ${response.statusText} ${JSON.stringify(data)} from: ${response.url}`);
+// };
 
-  const url = `${baseUrl}?env=test`;
-  const response = await fetch(url, {
-    method: 'GET',
-    headers,
-  });
-
-  // GOTCHA: must use .json() to convert response.body to json!!!
-  const data = await response.json();
-  console.log(`response: ${response.status} ${response.statusText} ${JSON.stringify(data)} from: ${response.url}`);
-};
-
-
-// updateFlag('dev-test-flag');
-listFlags();
+// listFlags();
+setInterval(() => {
+  run();
+}, 3000);

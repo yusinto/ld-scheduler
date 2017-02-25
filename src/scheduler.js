226ff3f72ca9a5b6cbb4315af91479b71214929a
Yusinto Ngadiman <yus@Yusintos-MacBook-Pro.local>
2017-02-25 12:35:35 +1100

    Added slack integration.

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,14 +1,19 @@
 import moment from 'moment';
 import config from 'config';
 
-const updateList = [
+const scheduledTasks = [
   {
-    flag: 'dev-test-flag',
+    flag: 'double-points-code',
     value: true,
-    from: '2017-02-24 23:00',
-    to: '2017-02-24 23:02', // TODO: update flag after we passed the 'to' date
-    isDeployed: false, // TODO: retrieve flag value from launch darkly to ascertain its value so we don't need this property
+    targetDeploymentDateTime: '2017-02-27 12:01',
+    isDeployed: false,
   },
+  {
+    flag: 'marketing-campaign-home-page',
+    value: true,
+    targetDeploymentDateTime: '2017-02-27 12:01',
+    isDeployed: false,
+  }
 ];
 const headers = {
   Accept: '*/*',
@@ -19,15 +24,15 @@ const headers = {
 
 // set said flag to the specified value
 const run = () => {
-  console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} lds is waking up...`);
-  const undeployedFlags = updateList.filter(f => !f.isDeployed && moment().isAfter(moment(f.from, 'YYYY-MM-DD HH:mm')));
+  console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} ld-scheduler is waking up with appEnv: ${config.appEnv}, ld.environment: ${config.launchDarkly.environment}`);
+  const outstandingTasks = scheduledTasks.filter(f => !f.isDeployed && moment().isAfter(moment(f.targetDeploymentDateTime, 'YYYY-MM-DD HH:mm')));
 
-  if (undeployedFlags.length === 0) {
+  if (outstandingTasks.length === 0) {
     console.log('Nothing to process, going back to sleep');
     return;
   }
 
-  undeployedFlags.forEach(async(f) => {
+  outstandingTasks.forEach(async(f) => {
     const {flag, value} = f;
     console.log(`Processing ${JSON.stringify(f)}`);
 
@@ -39,21 +44,55 @@ const run = () => {
     }]);
 
     const url = `${config.launchDarkly.rest.baseUrl}${config.launchDarkly.rest.flags}/${flag}`;
-    const response = await fetch(url, {
-      method: 'PATCH',
-      headers,
+
+    try {
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers,
+        body,
+      });
+
+      console.log(`LaunchDarkly api response: ${response.status} ${response.statusText} from: ${response.url}`);
+
+      if (response.status === 200) {
+        f.isDeployed = true;
+        console.log(`SUCCESS LD api! Updated ${flag} to ${value}.`);
+        messageSlack({isUpdateSuccessful: true, flag, value});
+      } else {
+        console.log(`LaunchDarkly threw an error. Did not update ${flag}. Will retry again later.`);
+        messageSlack({isUpdateSuccessful: false, flag, value});
+      }
+    } catch (e) {
+      console.log(`Network error. Could not reach LaunchDarkly. Did not update ${flag}. Will retry again later. ${e}`);
+      messageSlack({isUpdateSuccessful: false, flag, value});
+    }
+  });
+};
+
+const messageSlack = async ({isUpdateSuccessful, flag, value}) => {
+  let message;
+  const onOff = value ? 'on' : 'off';
+
+  if (isUpdateSuccessful) {
+    message = `Successfully switched ${onOff} ${flag}.`;
+  } else {
+    message = `FAILED to switch ${onOff} ${flag}. Will retry in a few minutes.`;
+  }
+
+  const body = JSON.stringify({
+    text: message,
+  });
+
+  try {
+    const response = await fetch(config.slack, {
+      method: 'POST',
       body,
     });
 
-    console.log(`Response: ${response.status} ${response.statusText} from: ${response.url}`);
-
-    if (response.status === 200) {
-      f.isDeployed = true;
-      console.log(`Successfully updated ${flag} to ${value}. `);
-    } else {
-      console.log(`Error occurred. Did not update ${flag}. Will retry again later.`);
-    }
-  });
+    console.log(`Posted message on slack. Response: ${response.status} ${response.statusText} from: ${response.url}`);
+  } catch (e) {
+    console.log(`Network error. Could not reach Slack. Did not post to slack regarding ${isUpdateSuccessful}, ${flag}: ${targetValue}. ${e}`);
+  }
 };
 
 // list all flags in the current project
